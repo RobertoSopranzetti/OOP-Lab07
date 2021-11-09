ea7c3d498b8e873da4790b340742b5739c539827
roberto.sopranzetti <roberto.sopranzetti@lab22-02-08.campusfc.dir.unibo.it>
2021-11-09 16:06:15 +0100

    Primo esercizio fatto

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -8,7 +10,7 @@ import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
 
 /**
- * Instruction: define two comparators as anonymous classes in oder to sort a
+ * Instruction: define two comparators as anonymous classes in order to sort a
  * list of user in the appropriate way.
  * 
  * 1) Study carefully the test in order to understand it
@@ -74,7 +76,19 @@ public final class TestAnonymousComparator {
          * 
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
-        // TODO
+        Collections.sort(denzelUsers, new Comparator<User>(){
+
+			public int compare(User o1, User o2) {
+				if (o1.getAge() < o2.getAge()) {
+					return -1;
+				} else if (o1.getAge() > o2.getAge()) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+        	
+        });
         /*
          * expected Result
          */
@@ -105,7 +119,20 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        Collections.sort(rossiUsers, new Comparator<User>() {
+
+			@Override
+			public int compare(User o1, User o2) {
+				if (o1.getAge() < o2.getAge()) {
+					return 1;
+				} else if (o1.getAge() > o2.getAge()) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+        	
+        });
         /*
          * expected Result
          */
