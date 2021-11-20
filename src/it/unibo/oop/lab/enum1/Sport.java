492f9aa6389562c64f1bb3634ff514b1e2452ab0
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2021-11-20 19:49:26 +0100

    Enum 1 completato

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -10,8 +10,11 @@ package it.unibo.oop.lab.enum1;
  * 
  */
 public enum Sport {
-    /*
-     * declare the following sports: - basket - soccer - tennis - bike - F1 -
-     * motogp - volley
-     */
+	BASKET, 
+	SOCCER, 
+	TENNIS, 
+	BIKE, 
+	FORMULA1, 
+	MOTOGP, 
+	VOLLEY 
 }
