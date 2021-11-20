0d288362fc3f001a234ed2dcf64b41775151dc76
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2021-11-20 22:00:22 +0100

    Lab 7 end

--- src/it/unibo/oop/lab/enum2/Sport.java
+++ src/it/unibo/oop/lab/enum2/Sport.java
@@ -37,7 +37,13 @@ public enum Sport {
      * - soccer
      * 
      */
-
+		BASKET(5, "Basketball", Place.INDOOR),
+		VOLLEYBALL(6, "Volleyball", Place.INDOOR),
+		TENNIS(1, "Tennis", Place.INDOOR),
+		BIKE(1, "Riding Bike", Place.OUTDOOR),
+		F1(1, "Formula 1", Place.OUTDOOR),
+		MOTOGP(1, "Moto GP", Place.OUTDOOR),
+		SOCCER(11, "Football", Place.OUTDOOR);
     /*
      * TODO
      * 
@@ -45,6 +51,10 @@ public enum Sport {
      * 
      * Declare required fields
      */
+		private final int members;
+		private final String name;
+		private final Place place;
+		
 
     /*
      * TODO
@@ -55,7 +65,11 @@ public enum Sport {
      * 
      * - Sport(final Place place, final int noTeamMembers, final String actualName)
      */
-
+		private Sport(final int members, final String name, final Place place) {
+			this.members = members;
+			this.name = name;
+			this.place = place;
+		}
     /*
      * TODO
      * 
@@ -65,20 +79,32 @@ public enum Sport {
      * 1) public boolean isIndividualSport()
      * 
      * Must return true only if called on individual sports
-     * 
-     * 
+     */
+		public boolean isIndividualSport() {
+			return this.members == 1;
+		}
+     /* 
      * 2) public boolean isIndoorSport()
      * 
      * Must return true in case the sport is practices indoor
-     * 
-     * 
+     */
+		public boolean isIndoorSport() {
+			return this.place == Place.INDOOR;
+		}
+     /* 
      * 3) public Place getPlace()
      * 
      * Must return the place where this sport is practiced
-     * 
-     * 
+     */
+		public Place getPlace() {
+			return this.place;
+		}
+     /* 
      * 4) public String toString()
      * 
      * Returns the string representation of a sport
      */
+		public String toString() {
+			return "Name: " + this.name + " Members: " + this.members + " is practiced " + this.place;
+		}
 }
